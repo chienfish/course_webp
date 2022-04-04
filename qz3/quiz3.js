3ca1e03e534874369503028289b062ffd805ab9c
林芊妤 <chienfish@ChienfishMacBook-Pro.local>
2022-04-05 00:49:31 +0800

    be synchronous

--- qz3/quiz3.js
+++ qz3/quiz3.js
@@ -1,6 +1,17 @@
 document.addEventListener('keydown', logkey);
 function logkey()
 {
-    var input = document.getElementById("input").value;
-    output.innerText = input;
+    var e = window.event;
+    // var str = String.fromCharCode(e.keyCode);
+    // var inputValue = document.getElementById("input").value;
+    var outputValue = document.getElementById("output");
+
+    if (e.key === "ArrowDown") {}
+    else if (e.key === "ArrowUp") {}
+    else if (e.key === "ArrowLeft") {}
+    else if (e.key === "ArrowRight") {}
+    else    
+        outputValue.innerText += e.key;
+    // inputValue.innerText += `${e.key}`;
+    // outputValue.innerText = inputValue;
 }
