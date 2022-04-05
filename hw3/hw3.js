89a5d20da18b608187c09d65d40c34db362ddffe
林芊妤 <chienfish@ChienfishMacBook-Pro.local>
2022-04-05 18:14:23 +0800

    add css, end game condition

--- hw3/hw3.js
+++ hw3/hw3.js
@@ -2,6 +2,9 @@ var contestArr = [];
 var i = 0;
 var contest = document.getElementById("random");
 var athlete = document.getElementById("input"); 
+var correct = document.getElementById("correct");
+var time = document.getElementById("time");
+var word = document.getElementById("word");
 
 function getRandomChar()    //隨機產生字母
 {   
@@ -10,19 +13,23 @@ function getRandomChar()    //隨機產生字母
     contest.innerText = str + contest.innerText;    //打字機
     contestArr[i] = str;
     i++;
-    
+
     return str;
 }
 
-for(var k = 0; k < 3; k++)      //開始時，打字機產生三個隨機字母
-    getRandomChar();
-
-
+document.addEventListener('keydown', create);
+function create()
+{
+    for (var k = 0; k < 3; k++)      //按下按鍵開始時，打字機產生三個隨機字母
+        getRandomChar();
+}
 
 var press = true;
 var time1 = 0;
 var time2 = 0;
 var totalTime = 0;
+var correctRes = 0;
+var timeRes = 0;
 
 document.addEventListener('keydown', logkey);
 function logkey()   
@@ -39,35 +46,43 @@ function logkey()
         var s = contestArr.reverse().join('');      //為了innerText
         contestArr.reverse(); 
         contest.innerText = s;
+
+        correctRes++;
+        correct.innerText = correctRes;
     }
 
-    if(press == true)
+    if (press == true)
     {
         time1 = new Date().getTime();
-        // console.log("time1", time1);
         press = !press;
     }
     else
     {
         time2 = new Date().getTime();
-        // console.log("time2", time2);
         press = !press;
     }
 
     var spendTime = Math.abs(time2 - time1);
-    if(time1 == 0 || time2 == 0)
+    if (time1 == 0 || time2 == 0)
         spendTime = 0;
-    // console.log("spendTime", spendTime);
     totalTime += spendTime;
 
-    // console.log("total", totalTime);
+    
+    timeRes += spendTime;
+    time.innerText = timeRes/1000;
 
-    while(totalTime >= 400)     //1秒2.5個字 => 0.4秒1個字
+    while (totalTime >= 400)     //1秒2.5個字 => 0.4秒1個字
     {
         getRandomChar();
         totalTime -= 400;
+
+
+        word.innerText = i;
+        if (i >= 50)
+        {
+            alert(" !! Competition is Over !! ");
+            break;
+        }
+        
     }
-    // console.log("rest", totalTime);
-    
-    // console.log("--");
 }
