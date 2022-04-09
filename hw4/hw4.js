24c38dde837ecfb723cb40b2fbe877c3706d762c
林芊妤 <chienfish@ChienfishMacBook-Pro.local>
2022-04-10 02:33:43 +0800

    get title of .jpg

--- hw4/hw4.js
+++ hw4/hw4.js
@@ -1,23 +1,58 @@
-var jsImg = new Array("./g.jpg", "./u.jpg", "./v.jpg", "./i.jpg", "./x.jpg", "./l.jpg", "./n.jpg", "./w.jpg", "./y.jpg", "./z.jpg");
+var jsImg = new Array("./G.jpg", "./U.jpg", "./V.jpg", "./I.jpg", "./X.jpg", "./L.jpg", "./N.jpg", "./Y.jpg", "./Z.jpg", "./P.jpg", "./S.jpg");
+var imgArr1 = [];
+var imgArr2 = [];
+var imgArr3 = [];
+var imgArr4 = [];
+var imgArr5 = [];
+var imgArr6 = [];
+var index1 = Math.floor(Math.random()*jsImg.length);
+var index2 = Math.floor(Math.random()*jsImg.length);
+var index3 = Math.floor(Math.random()*jsImg.length);
+var index4 = Math.floor(Math.random()*jsImg.length);
+var index5 = Math.floor(Math.random()*jsImg.length);
+var index6 = Math.floor(Math.random()*jsImg.length);
+
+function splitPart(index)
+{
+    var splitIndex = index.split(/[./.jpg]/);
+    var joinIndex = splitIndex.join(' ');
+    var result = String(joinIndex).trim()
+    // console.log(result);
+
+    return result;
+}
+
 //設定每x秒執行一次randomImg() ，此行要在 function 之外
-setInterval("randomImg()",100);
+// setInterval("randomImg()",100);
 function randomImg()
 {
-    //陣列的長度 * 介於0~1間數字 ，然後在取 floor 當照片索引值
-    var imgIndex1 = Math.floor(Math.random()*jsImg.length);
-    var imgIndex2 = Math.floor(Math.random()*jsImg.length);
-    var imgIndex3 = Math.floor(Math.random()*jsImg.length);
-    var imgIndex4 = Math.floor(Math.random()*jsImg.length);
-    var imgIndex5 = Math.floor(Math.random()*jsImg.length);
-    var imgIndex6 = Math.floor(Math.random()*jsImg.length);
-    $(document).ready(function(){
-        $(document).keydown(function(){
-            $("#create1").html("<img src='"+jsImg[imgIndex1]+"' width=60>");
-            $("#create2").html("<img src='"+jsImg[imgIndex2]+"' width=60>");
-            $("#create3").html("<img src='"+jsImg[imgIndex3]+"' width=60>");
-            $("#create4").html("<img src='"+jsImg[imgIndex4]+"' width=60>");
-            $("#create5").html("<img src='"+jsImg[imgIndex5]+"' width=60>");
-            $("#create6").html("<img src='"+jsImg[imgIndex6]+"' width=60>");
-        });
-    });
+    $("#create1").html("<img src='"+jsImg[index1]+"' width=60>");
+        imgArr1.push(jsImg[index1]);
+        splitPart(jsImg[index1]);
+    $("#create2").html("<img src='"+jsImg[index2]+"' width=60>");
+        imgArr2.push(jsImg[index2]);
+        splitPart(jsImg[index2]);
+    $("#create3").html("<img src='"+jsImg[index3]+"' width=60>");
+        imgArr3.push(jsImg[index3]);
+        splitPart(jsImg[index3]);
+    $("#create4").html("<img src='"+jsImg[index4]+"' width=60>");
+        imgArr4.push(jsImg[index4]);
+        splitPart(jsImg[index4]);
+    $("#create5").html("<img src='"+jsImg[index5]+"' width=60>");
+        imgArr5.push(jsImg[index5]);
+        splitPart(jsImg[index5]);
+    $("#create6").html("<img src='"+jsImg[index6]+"' width=60>");
+        imgArr6.push(jsImg[index6]);
+        splitPart(jsImg[index6]);
 } 
+
+
+
+
+$(document).ready(function()
+{
+    $(document).keydown(function()
+    {
+        randomImg();
+    });
+}); 
