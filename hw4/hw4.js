2985f5e0d6de54c9f3893705e8e0ad75834a97ac
林芊妤 <chienfish@ChienfishMacBook-Pro.local>
2022-04-10 04:24:17 +0800

    remove normally

--- hw4/hw4.js
+++ hw4/hw4.js
@@ -24,47 +24,40 @@ function randomImg()
 {
     $("#create1").html("<img src='" +jsImg[index1]+"' width = 60>");
         imgArr1[i1] = splitPart(jsImg[index1]);
-        console.log(i1, imgArr1[i1]);
         i1++;
     $("#create2").html("<img src='" +jsImg[index2]+"' width = 60>");
         imgArr2[i2] = splitPart(jsImg[index2]);
-        console.log(i2, imgArr2[i2]);
         i2++;
     $("#create3").html("<img src='" +jsImg[index3]+"' width = 60>");
         imgArr3[i3] = splitPart(jsImg[index3]);
-        console.log(i3, imgArr3[i3]);
         i3++;
     $("#create4").html("<img src='" +jsImg[index4]+"' width = 60>");
         imgArr4[i4] = splitPart(jsImg[index4]);
-        console.log(i4, imgArr4[i4]);
         i4++;
     $("#create5").html("<img src='" +jsImg[index5]+"' width = 60>");
         imgArr5[i5] = splitPart(jsImg[index5]);
-        console.log(i5, imgArr5[i5]);
         i5++;
     $("#create6").html("<img src='" +jsImg[index6]+"' width = 60>");
         imgArr6[i6] = splitPart(jsImg[index6]);
-        console.log(i6, imgArr6[i6]);
         i6++;
 } 
 
-function cmpNdel(str, imgArr, i)
+function delStr(imgArr, i)
 {
-    if (str === imgArr[0]){
-        imgArr.shift();
-        i--;
-
-        switch (imgArr){
-            case imgArr1: $("#create1").remove(); break;
-            case imgArr2: $("#create2").remove(); break;
-            case imgArr3: $("#create3").remove(); break;
-            case imgArr4: $("#create4").remove(); break;
-            case imgArr5: $("#create5").remove(); break;
-            case imgArr6: $("#create6").remove(); break;
-            default: break;
-        }
+    imgArr.shift();
+    i--;
+    switch (imgArr){
+        case imgArr1: $("#create1").remove(); break;
+        case imgArr2: $("#create2").remove(); break;
+        case imgArr3: $("#create3").remove(); break;
+        case imgArr4: $("#create4").remove(); break;
+        case imgArr5: $("#create5").remove(); break;
+        case imgArr6: $("#create6").remove(); break;
+        default: break;
     }
 }
+
+randomImg();
 
 $(document).ready(function(){
     $(document).keydown(function(){
@@ -72,13 +65,19 @@ $(document).ready(function(){
         var str = String.fromCharCode(e.keyCode);
         $("#input").append(str);
 
-        randomImg();
-        cmpNdel(str, imgArr1, i1);
-        cmpNdel(str, imgArr2, i2);
-        cmpNdel(str, imgArr3, i3);
-        cmpNdel(str, imgArr4, i4);
-        cmpNdel(str, imgArr5, i5);
-        cmpNdel(str, imgArr6, i6);
+        if (str === imgArr1[0]) delStr(imgArr1, i1);
+        else if (str === imgArr2[0]) delStr(imgArr2, i2);
+        else if (str === imgArr3[0]) delStr(imgArr3, i3);
+        else if (str === imgArr4[0]) delStr(imgArr4, i4);
+        else if (str === imgArr5[0]) delStr(imgArr5, i5);
+        else if (str === imgArr6[0]) delStr(imgArr6, i6);
         
+        
+        console.log("1", imgArr1);
+        console.log("2", imgArr2);
+        console.log("3", imgArr3);
+        console.log("4", imgArr4);
+        console.log("5", imgArr5);
+        console.log("6", imgArr6);
     });
 });
