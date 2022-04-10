219a4819b6cb4a5dcf259c481eabe679c524e7e7
林芊妤 <chienfish@ChienfishMacBook-Pro.local>
2022-04-10 23:40:29 +0800

    except animate

--- hw4/hw4.js
+++ hw4/hw4.js
@@ -23,32 +23,32 @@ function randomImg(){
     var index5 = Math.floor(Math.random()*jsImg.length);
     var index6 = Math.floor(Math.random()*jsImg.length);
 
-    $("#create1").prepend("<img src='" +jsImg[index1]+"' width = 60>");
+    $("#create1").prepend("<img src='" +jsImg[index1]+"' width = 55>");
         imgArr1.push(splitPart(jsImg[index1]));
         i1++;
         indexArr1[id1] = index1;
         id1++;
-    $("#create2").prepend("<img src='" +jsImg[index2]+"' width = 60>");
+    $("#create2").prepend("<img src='" +jsImg[index2]+"' width = 55>");
         imgArr2.push(splitPart(jsImg[index2]));
         i2++;
         indexArr2[id2] = index2;
         id2++;
-    $("#create3").prepend("<img src='" +jsImg[index3]+"' width = 60>");
+    $("#create3").prepend("<img src='" +jsImg[index3]+"' width = 55>");
         imgArr3.push(splitPart(jsImg[index3]));
         i3++;
         indexArr3[id3] = index3;
         id3++;
-    $("#create4").prepend("<img src='" +jsImg[index4]+"' width = 60>");
+    $("#create4").prepend("<img src='" +jsImg[index4]+"' width = 55>");
         imgArr4.push(splitPart(jsImg[index4]));
         i4++;
         indexArr4[id4] = index4;
         id4++;
-    $("#create5").prepend("<img src='" +jsImg[index5]+"' width = 60>");
+    $("#create5").prepend("<img src='" +jsImg[index5]+"' width = 55>");
         imgArr5.push(splitPart(jsImg[index5]));
         i5++;
         indexArr5[id5] = index5;
         id5++;
-    $("#create6").prepend("<img src='" +jsImg[index6]+"' width = 60>");
+    $("#create6").prepend("<img src='" +jsImg[index6]+"' width = 55>");
         imgArr6.push(splitPart(jsImg[index6]));
         i6++;
         indexArr6[id6] = index6;
@@ -63,7 +63,7 @@ function cmpNdel(str){
         id1--;
         $("#create1").html("");
         for (var i = 0; i < id1; i++)
-            $("#create1").prepend("<img src='" +jsImg[indexArr1[i]]+"' width = 60>");
+            $("#create1").prepend("<img src='" +jsImg[indexArr1[i]]+"' width = 55>");
     } else if (str === imgArr2[0]){
         imgArr2.shift();
         i2--;
@@ -71,7 +71,7 @@ function cmpNdel(str){
         id2--;
         $("#create2").html("");
         for (var i = 0; i < id2; i++)
-            $("#create2").prepend("<img src='" +jsImg[indexArr2[i]]+"' width = 60>");
+            $("#create2").prepend("<img src='" +jsImg[indexArr2[i]]+"' width = 55>");
     } else if (str === imgArr3[0]){
         imgArr3.shift();
         i3--;
@@ -79,7 +79,7 @@ function cmpNdel(str){
         id3--;
         $("#create3").html("");
         for (var i = 0; i < id3; i++)
-            $("#create3").prepend("<img src='" +jsImg[indexArr3[i]]+"' width = 60>");
+            $("#create3").prepend("<img src='" +jsImg[indexArr3[i]]+"' width = 55>");
     } else if (str === imgArr4[0]){
         imgArr4.shift();
         i4--;
@@ -87,7 +87,7 @@ function cmpNdel(str){
         id4--;
         $("#create4").html("");
         for (var i = 0; i < id4; i++)
-            $("#create4").prepend("<img src='" +jsImg[indexArr4[i]]+"' width = 60>");
+            $("#create4").prepend("<img src='" +jsImg[indexArr4[i]]+"' width = 55>");
     } else if (str === imgArr5[0]){
         imgArr5.shift();
         i5--;
@@ -95,7 +95,7 @@ function cmpNdel(str){
         id5--;
         $("#create5").html("");
         for (var i = 0; i < id5; i++)
-            $("#create5").prepend("<img src='" +jsImg[indexArr5[i]]+"' width = 60>");
+            $("#create5").prepend("<img src='" +jsImg[indexArr5[i]]+"' width = 55>");
     } else if (str === imgArr6[0]){
         imgArr6.shift();
         i6--;
@@ -103,7 +103,7 @@ function cmpNdel(str){
         id6--;
         $("#create6").html("");
         for (var i = 0; i < id6; i++)
-            $("#create6").prepend("<img src='" +jsImg[indexArr6[i]]+"' width = 60>");
+            $("#create6").prepend("<img src='" +jsImg[indexArr6[i]]+"' width = 55>");
     }
 }
 
@@ -132,10 +132,25 @@ $(document).ready(function(){
             spendTime = 0;
         totalTime += spendTime;
 
-        while (totalTime >= 3000){     //1秒2.5個字 => 0.67秒1個字
+        while (totalTime >= 670){     //1秒1.5個字 => 0.67秒1個字
             randomImg();
-            totalTime -= 3000;
+            totalTime -= 670;
 
+            if (((id1 >= 16 || id2 >= 16)||(id3 >= 16 || id4 >= 16))||(id5 >= 16 || id6 >= 16)){
+                alert("!! Game Over !!");
+                break; 
+            }
         }
+    });
+});
+
+$(document).ready(function(){
+    window.setInterval(function(){
+        $("#create1").animate({left: "+=10px"});
+        $("#create2").animate({left: "+=10px"});
+        $("#create3").animate({left: "+=10px"});
+        $("#create4").animate({left: "+=10px"});
+        $("#create5").animate({left: "+=10px"});
+        $("#create6").animate({left: "+=10px"});
     });
 });
