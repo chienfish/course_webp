7b1bd7010642131b4e980751656e8ba5314fb680
林芊妤 <chienfish@ChienfishMacBook-Pro.local>
2022-04-24 04:19:45 +0800

    add time

--- hw5/hw5.js
+++ hw5/hw5.js
@@ -1,10 +1,33 @@
+// var timeData = "https://ptx.transportdata.tw/MOTC/v2/Rail/Metro/S2STravelTime/TYMC?%24format=JSON";
+var time = $.getJSON("./time.json");
+var times = [], nth = [];
+var num = 0;
+var countDown = 20;
+for(var i = 0; i < 20; i++){
+    if(countDown > 0){
+        nth.push(num);
+        num = num + countDown;    //+20 +19 +18 +17 ...
+        countDown--;
+    }
+    // console.log(i, nth[i]);
+}
+time.done(function(result){
+    $.each(result, function(i, index){
+        if (i == 0){
+            $.each(nth,function(j, nth){
+                times[j] = index.TravelTimes[nth].RunTime;
+            });
+        }
+    });
+});
+
 // var priceData = "https://ptx.transportdata.tw/MOTC/v2/Rail/Metro/ODFare/TYMC?%24format=JSON";
 var price = $.getJSON("./price.json");
 var prices = [];
 var x = 0;
 price.done(function(result){
     $.each(result, function(i, index){
-        if (i % 21 == 0){
+        if (i % 21 == 0){       //0 21 42 63 84 ...
             prices[x] = index.Fares[0].Price;
             x++;
             // console.log(i);
@@ -21,6 +44,8 @@ station.done(function(result){
             return false;
         }
         else
-            $("#station").append("A" + (i+1) + index.StationName.Zh_tw + "<br/><br/>" + "&emsp;&ensp;&emsp;⇣&emsp;&emsp;$" +prices[i]+ "<br/><br/>");
+            $("#station").append("A" + (i+1) + index.StationName.Zh_tw + "<br/><br/>$" 
+                        +prices[i]+ "&emsp;⇣&emsp;" 
+                        +times[i]/60 + "分"+ times[i]%60 +"秒<br/><br/>");
     });
 });
