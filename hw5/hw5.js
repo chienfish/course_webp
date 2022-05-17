806813ce2e12deebcf1c84e7fbea38a9faab3c3d
林芊妤 <chienfish@ChienfishMacBook-Pro.local>
2022-05-17 14:26:16 +0800

    modify price

--- hw5/hw5.js
+++ hw5/hw5.js
@@ -1,6 +1,6 @@
-var timeData = "https://ptx.transportdata.tw/MOTC/v2/Rail/Metro/S2STravelTime/TYMC?%24format=JSON";
-var time = $.getJSON(timeData);
-// var time = $.getJSON("./time.json");
+// var timeData = "https://ptx.transportdata.tw/MOTC/v2/Rail/Metro/S2STravelTime/TYMC?%24format=JSON";
+// var time = $.getJSON(timeData);
+var time = $.getJSON("./time.json");
 var times = [], nth = [];
 var num = 0;
 var countDown = 20;
@@ -22,32 +22,55 @@ time.done(function(result){
     });
 });
 
-var priceData = "https://ptx.transportdata.tw/MOTC/v2/Rail/Metro/ODFare/TYMC?%24format=JSON";
-var price = $.getJSON(priceData);
-// var price = $.getJSON("./price.json");
+// var priceData = "https://ptx.transportdata.tw/MOTC/v2/Rail/Metro/ODFare/TYMC?%24format=JSON";
+// var price = $.getJSON(priceData);
+var price = $.getJSON("./price.json");
+var order = [];
+for (var i = 0; i < 22; i++)
+{
+    for (var j = 1; j < i; j++)
+        order.push((j-1) * 20 +i-2);
+}
+
 var prices = [];
-var x = 0;
 price.done(function(result){
-    $.each(result, function(i, index){
-        if (i % 21 == 0){       //0 21 42 63 84 ...
-            prices[x] = index.Fares[0].Price;
-            x++;
-            // console.log(i);
-        }
-    });
+    for (var i = 0; i < 210; i++)
+    {
+        $.each(result, function(j, index){
+            if (j == order[i])
+                prices[i] = index.Fares[0].Price;
+        });
+        console.log(i, order[i], prices[i]);
+    }
+    
 });
 
-var stationData = "https://ptx.transportdata.tw/MOTC/v2/Rail/Metro/Station/TYMC?%24top=30&%24format=JSON";
-var station = $.getJSON(stationData);
-// var station = $.getJSON("./station.json");
+
+
+// var stationData = "https://ptx.transportdata.tw/MOTC/v2/Rail/Metro/Station/TYMC?%24top=30&%24format=JSON";
+// var station = $.getJSON(stationData);
+var station = $.getJSON("./station.json");
+var y = 0;
 station.done(function(result){
     $.each(result, function(i, index){
+        $("#stationPrice").append("A" + (i+1) + index.StationName.Zh_tw + "<br/>");
+        for (var j = -1; j < i; j++)
+        {
+            if (i == 20) break;
+            
+            if (prices[y] < 100) $("#stationPrice").append(prices[y] + "&nbsp;&nbsp;&emsp;&emsp;");
+            else $("#stationPrice").append(prices[y] + "&emsp;&emsp;");
+            y++;
+        }
+    });
+
+    $.each(result, function(i, index){
         if (i == 20){
-            $("#station").append("A" + (i+1) + index.StationName.Zh_tw + "<br/>");
+            $("#stationTime").append("A" + (i+1) + index.StationName.Zh_tw + "<br/>");
             return false;
         }
         else
-            $("#station").append("A" + (i+1) + index.StationName.Zh_tw + "<br/><br/>$" 
+            $("#stationTime").append("A" + (i+1) + index.StationName.Zh_tw + "<br/><br/>$" 
                         +prices[i]+ "&emsp;⇣&emsp;" 
                         +times[i]/60 + "分"+ times[i]%60 +"秒<br/><br/>");
     });
